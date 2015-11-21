3dfb62853ce6295b6a3ad6c5ffc635368a6b9b53
Edmanuel Torres <eetorres>
2015-11-21 12:56:13 -0500

    version

--- include/version.h
+++ include/version.h
@@ -4,10 +4,10 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "16";
-	static const char MONTH[] = "07";
+	static const char DATE[] = "21";
+	static const char MONTH[] = "11";
 	static const char YEAR[] = "2015";
-	static const char UBUNTU_VERSION_STYLE[] =  "15.07";
+	static const char UBUNTU_VERSION_STYLE[] =  "15.11";
 	
 	//Software Status
 	static const char STATUS[] =  "Alpha";
@@ -17,16 +17,16 @@ namespace AutoVersion{
 	static const long MAJOR  = 1;
 	static const long MINOR  = 0;
 	static const long BUILD  = 8;
-	static const long REVISION  = 7;
+	static const long REVISION  = 8;
 	
 	//Miscellaneous Version Types
 	static const long BUILDS_COUNT  = 0;
-	#define RC_FILEVERSION 1,0,8,7
-	#define RC_FILEVERSION_STRING "1, 0, 8, 7\0"
-	static const char FULLVERSION_STRING [] = "1.0.8.7";
+	#define RC_FILEVERSION 1,0,8,8
+	#define RC_FILEVERSION_STRING "1, 0, 8, 8\0"
+	static const char FULLVERSION_STRING [] = "1.0.8.8";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 43;
+	static const long BUILD_HISTORY  = 44;
 	
 
 }
