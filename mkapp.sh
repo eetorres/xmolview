53ce34d4f3ca434251aacf5c1520a68be33d1ad5
Edmanuel Torres <etorres@dirac.local>
2016-02-27 09:42:11 -0500

    mac app

--- mkapp.sh
+++ mkapp.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+rm -Rf ./xmolview.app
 mkdir ./xmolview.app
 mkdir ./xmolview.app/Contents
 mkdir ./xmolview.app/Contents/Resources
 mkdir ./xmolview.app/Contents/MacOS
 echo APPLnone > ./xmolview.app/Contents/PkgInfo
-cp bin/xmolview ./xmolview.app/Contents/MacOS
+iconutil -c icns ./res/xmolview.iconset/
+cp ./src/xmolview ./xmolview.app/Contents/MacOS
+cp ./res/xmolview.icns ./xmolview.app/Contents/Resources/
 chmod 755 ./xmolview.app/Contents/MacOS/xmolview
 cat << EOF > ./xmolview.app/Contents/info.plist
 <?xml version="1.0" encoding="UTF-8"?>
@@ -21,7 +24,7 @@ cat << EOF > ./xmolview.app/Contents/info.plist
         <key>CFBundleShortVersionString</key>
         <string>1.1</string>
         <key>CFBundleIconFile</key>
-        <string>icon.icns</string>
+        <string>xmolview.icns</string>
         <key>CFBundleSignature</key>
         <string>none</string>
 </dict>
