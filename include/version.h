0706bf4897eb817497f6da8cc49ab0a99ec054bd
eetorres <eetorres@gmail.com>
2016-03-05 13:47:55 -0500

    new version

--- include/version.h
+++ include/version.h
@@ -1,33 +1,35 @@
+
 #ifndef VERSION_H
 #define VERSION_H
 
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "13";
-	static const char MONTH[] = "12";
-	static const char YEAR[] = "2015";
-	static const char UBUNTU_VERSION_STYLE[] =  "15.12";
+	static const char DATE[] = "05";
+	static const char MONTH[] = "03";
+	static const char YEAR[] = "2016";
+	static const char UBUNTU_VERSION_STYLE[] = "16.03";
 	
 	//Software Status
-	static const char STATUS[] =  "Alpha";
-	static const char STATUS_SHORT[] =  "a";
+	static const char STATUS[] = "Alpha";
+	static const char STATUS_SHORT[] = "a";
 	
 	//Standard Version Type
-	static const long MAJOR  = 1;
-	static const long MINOR  = 0;
-	static const long BUILD  = 9;
-	static const long REVISION  = 2;
+	static const long MAJOR = 1;
+	static const long MINOR = 0;
+	static const long BUILD = 9;
+	static const long REVISION = 3;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 0;
-	#define RC_FILEVERSION 1,0,9,2
-	#define RC_FILEVERSION_STRING "1, 0, 9, 2\0"
-	static const char FULLVERSION_STRING [] = "1.0.9.2";
+	static const long BUILDS_COUNT = 0;
+	#define RC_FILEVERSION 1,0,9,3
+	#define RC_FILEVERSION_STRING "1, 0, 9, 3\0"
+	static const char FULLVERSION_STRING [] = "1.0.9.3";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 0;
+	static const long BUILD_HISTORY = 2;
 	
 
 }
 #endif //VERSION_H
+
